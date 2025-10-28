c0d9cb0e27fe0f2fe1382c75e182e510fc0e302e
Pedro Moraes <pmoraes.rogerio@gmail.com>
2025-10-28 07:43:06 -0300

    3: Alteração para baixar items do outlook dentro do e-mail

--- baixarAnexo.py
+++ baixarAnexo.py
@@ -1,26 +1,32 @@
 import os
 import win32com.client
 
-download_folder = r'C:\Users\pedro_moraes\Downloads'
+download_folder = r"C:\Users\pedro_moraes\Downloads"
 
-outlook = win32com.client.Dispatch('Outlook.Application').GetNameSpace('MAPI')
-inbox = outlook.GetDefaultFolder(6)
+outlook = win32com.client.Dispatch("Outlook.Application").GetNameSpace("MAPI")
+inbox = outlook.GetDefaultFolder(6)  # Caixa de Entrada
 
-target_keyword = 'teste456'
+target_keyword = "teste456"
+
 
 def search_folder(folder):
     try:
         messages = folder.Items
         for message in messages:
             if message.Class == 43:  # MailItem
-                if target_keyword in message.Subject:  
+                if target_keyword in message.Subject:
                     for i in range(1, message.Attachments.Count + 1):
                         attachment = message.Attachments.Item(i)
-                        if attachment.FileName.lower().endswith('.pdf'):
-                            save_path = os.path.join(download_folder, attachment.FileName)
-                            attachment.SaveAsFile(save_path)  
-                            print(f"Arquivo baixado de '{folder.Name}': {attachment.FileName}")
+                        if attachment.FileName.lower().endswith(".pdf"):
+                            save_path = os.path.join(
+                                download_folder, attachment.FileName
+                            )
+                            attachment.SaveAsFile(save_path)
+                            print(
+                                f"Arquivo baixado de '{folder.Name}': {attachment.FileName}"
+                            )
     except Exception as e:
         print(f"Erro na pasta: '{folder.Name}': {str(e)}")
+
 
 search_folder(inbox)
